0cabc5dd682073f525701237e87ad5aac7ea5a57
harshavardanan <harshamoorthy22@gmail.com>
2025-02-21 22:20:21 +0000

    route needs fixing

--- server/Routes/Users.js
+++ server/Routes/Users.js
@@ -19,7 +19,7 @@ router.post("/", async (req, res) => {
       email,
       name,
     });
-    res.status(200).json(user).json("user saved");
+    res.status(200).json(user);
   } catch (err) {
     res.status(400).json({ message: err });
   }
