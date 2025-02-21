758af7af78d76882ecf753a1a8c7bcd9d49359b5
harshavardanan <harshamoorthy22@gmail.com>
2025-02-21 21:35:27 +0000

    socket latency fixed

--- client/src/components/SocketConfig.tsx
+++ client/src/components/SocketConfig.tsx
@@ -1,39 +1,48 @@
 import React, { useState, useEffect } from "react";
 import TextEditor from "./TextEditor";
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
 const ENDPOINT = "http://localhost:5000";
 
-const SocketConfig = () => {
+const SocketConfig: React.FC = () => {
   const [editorData, setEditorData] = useState<string>("");
-  
+  const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
-    const socket = io(ENDPOINT);
-    socket.on("connect", () => {
+    // Initialize the socket connection
+    const newSocket = io(ENDPOINT);
+    setSocket(newSocket);
+
+    // Handle connection
+    newSocket.on("connect", () => {
       console.log("Connected to server");
-      socket.on("join-room", (room) => {
-        console.log("Joined room", room);
-      });
-      socket.on("editing", (data: string) => {
-        console.log("data received from server", data);
-        setEditorData(data);
+
+      // Join a room (e.g., "room1")
+      newSocket.emit("join-room", { room: "room1" });
+
+      // Listen for editing events
+      newSocket.on("editing", (data: string) => {
+        console.log("Data received from server:", data);
+        setEditorData(data); // Update the editor content
       });
     });
 
+    // Cleanup on unmount
     return () => {
-      socket.disconnect();
+      newSocket.disconnect();
     };
   }, []);
 
-  const sendChanges = (editorData: string) => {
-    console.log("sending data to server", editorData);
-    io(ENDPOINT).emit("editing", editorData);
+  // Send changes to the server
+  const sendChanges = (data: string) => {
+    console.log("Sending data to server:", data);
+    if (socket) {
+      socket.emit("editing", { room: "room1", data }); // Emit to the room
+    }
   };
 
   return (
     <div>
-      
       <TextEditor editorData={editorData} setEditorData={sendChanges} />
     </div>
   );
--- server/SocketConfig.js
+++ server/SocketConfig.js
@@ -1,7 +1,3 @@
-// const app = require("express")();
-// const server = require("http").createServer(app);
-// const cors = require("cors");
-// app.use(cors());
 const socketIo = require("socket.io");
 const { addUser, removeUser, getUser, getUsersInRoom } = require("./Users");
 
@@ -38,51 +34,3 @@ const connectSocket = (server, cors) => {
 };
 
 module.exports = connectSocket;
-
-// const socketIo = require("socket.io");
-// const { addUser, removeUser, getUser, getUsersInRoom } = require("./Users");
-
-// const connectSocket = (server) => {
-//   const io = socketIo(server, { cors: { origin: "*" } }); // Initialize `io` with the `server`
-
-//   io.on("connection", (socket) => {
-//     console.log(`User connected: ${socket.id}`);
-
-//     socket.on("editing", ({ room, data }) => {
-//       console.log(`Editing event received in room ${room}`);
-//       socket.to(room).emit("editing", data);
-//     });
-
-//     socket.on("join-room", ({ room }) => {
-//       console.log(`User ${socket.id} is joining room ${room}`);
-//       const user = addUser({ id: socket.id, room });
-
-//       if (!user) return;
-
-//       socket.join(room);
-
-//       io.to(user.room).emit("user-joiner", {
-//         userId: user.id,
-//         message: `User ${user.id} joined the room`,
-//       });
-
-//       io.to(user.room).emit("room-data", {
-//         room: user.room,
-//         users: getUsersInRoom(user.room),
-//       });
-
-//       console.log(`User ${user.id} joined room ${user.room}`);
-//     });
-
-//     socket.on("disconnect", () => {
-//       console.log(`User disconnected: ${socket.id}`);
-//       removeUser(socket.id); // Remove the user when they disconnect
-//       io.to(user.room).emit("room-data", {
-//         room: user.room,
-//         users: getUsersInRoom(user.room),
-//       });
-//     });
-//   });
-// };
-
-// module.exports = connectSocket;
